6e3bf76ff76c85751d588653fcde0c2f7475c53e
Tahani50 <tahanyayman4@gmail.com>
2025-06-15 14:03:55 +0300

    Initial Commit

---
+++ FoodRecipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodRecipeApp
+//
+//  Created by Tahani Ayman on 19/12/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodRecipeApp/FoodRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeAppApp.swift
+//  FoodRecipeApp
+//
+//  Created by Tahani Ayman on 19/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
